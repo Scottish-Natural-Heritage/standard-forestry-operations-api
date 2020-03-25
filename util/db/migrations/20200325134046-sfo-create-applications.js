0af87b5424a11e0d1b6908bda274d0e57e6b66ca
Mike Coats <mike.coats@nature.scot>
2020-03-25 15:00:07 +0000

    Create Applications table
    Issue: Scottish-Natural-Heritage/Licensing#288

---
+++ util/db/migrations/20200325134046-sfo-create-applications.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Applications', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      convictions: {
+        type: Sequelize.BOOLEAN
+      },
+      complyWithTerms: {
+        type: Sequelize.BOOLEAN
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      companyOrganisation: {
+        type: Sequelize.STRING
+      },
+      addressLine1: {
+        type: Sequelize.STRING
+      },
+      addressLine2: {
+        type: Sequelize.STRING
+      },
+      addressTown: {
+        type: Sequelize.STRING
+      },
+      addressCounty: {
+        type: Sequelize.STRING
+      },
+      addressPostcode: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      emailAddress: {
+        type: Sequelize.STRING
+      },
+      siteName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Applications');
+  }
+};
