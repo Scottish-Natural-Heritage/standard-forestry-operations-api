cfbb5854fb25afe5fe278e225cfda64d9091ae4e
Mike Coats <mike.coats@nature.scot>
2020-03-27 11:50:49 +0000

    Add migrations for the setts table
    Include a meta table and set up the foreign key associations.
    
    Issue: Scottish-Natural-Heritage/Licensing#289

---
+++ util/db/migrations/20200326111631-sfo-create-setts-types.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Create the SettTypes meta-table
+    await queryInterface.createTable('SettTypes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      name: {
+        type: Sequelize.STRING
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+    // Fill the SettTypes meta-table
+    await queryInterface.bulkInsert('SettTypes', [
+      {
+        id: 1,
+        name: 'Main Sett',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        name: 'Outlying Sett',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 3,
+        name: 'Annex Sett',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 4,
+        name: 'Subsidiary Sett',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('SettTypes', {}, {truncate: true});
+    await queryInterface.dropTable('SettTypes');
+  }
+};
