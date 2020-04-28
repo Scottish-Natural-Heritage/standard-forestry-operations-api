99df0e728211cc228586144dba61da62feb8425a
Mike Coats <mike.coats@nature.scot>
2020-04-28 14:48:12 +0100

    Add the model for saving emails for later
    Issue: Scottish-Natural-Heritage/Licensing#310

--- src/models/index.js
+++ src/models/index.js
@@ -3,12 +3,14 @@ import Sequelize from 'sequelize';
 
 import Application from './application.js';
 import Sett from './sett.js';
+import ApplyOther from './apply-other.js';
 
 const sequelize = new Sequelize(databaseConfig.production);
 
 const database = {
   Application: Application(sequelize),
-  Sett: Sett(sequelize)
+  Sett: Sett(sequelize),
+  ApplyOther: ApplyOther(sequelize),
 };
 
 database.Application.hasMany(database.Sett);
