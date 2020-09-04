a389eb79657dde2bffec4c2e73032e0b09db4453
Mike Coats <mike.coats@nature.scot>
2020-09-04 08:46:59 +0100

    Turn off pre-migrations for sqlite
    Issue: Scottish-Natural-Heritage/Licensing#395

--- util/db/pre-migrations/20200325114109-sfo-grant-to-user.js
+++ util/db/pre-migrations/20200325114109-sfo-grant-to-user.js
@@ -1,27 +1,40 @@
 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    // Allow our app's user to connect to the database.
-    await queryInterface.sequelize.query('grant connect on database licensing to sfo;', {
-      type: Sequelize.QueryTypes.RAW
-    });
+// The pre-migrations only make sense when running inside the production docker
+// environment. They are not required for the development SQLite DB.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      // Allow our app's user to connect to the database.
+      await queryInterface.sequelize.query('grant connect on database licensing to sfo;', {
+        type: Sequelize.QueryTypes.RAW
+      });
 
-    // Grant our app's user full control of their own schema.
-    await queryInterface.sequelize.query('grant all on schema sfo to sfo;', {
-      type: Sequelize.QueryTypes.RAW
-    });
-  },
+      // Grant our app's user full control of their own schema.
+      await queryInterface.sequelize.query('grant all on schema sfo to sfo;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    // Revoke our app's user's privileges from its schema.
-    await queryInterface.sequelize.query('revoke all on schema sfo from sfo;', {
-      type: Sequelize.QueryTypes.RAW
-    });
+    down: async (queryInterface, Sequelize) => {
+      // Revoke our app's user's privileges from its schema.
+      await queryInterface.sequelize.query('revoke all on schema sfo from sfo;', {
+        type: Sequelize.QueryTypes.RAW
+      });
 
-    // Prevent our app's user from connecting to the database.
-    await queryInterface.sequelize.query('revoke all on database licensing from sfo;', {
-      type: Sequelize.QueryTypes.RAW
-    });
-  }
-};
+      // Prevent our app's user from connecting to the database.
+      await queryInterface.sequelize.query('revoke all on database licensing from sfo;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    }
+  };
+} else {
+  module.exports = {
+    up: () => {
+      return Promise.resolve();
+    },
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+}
