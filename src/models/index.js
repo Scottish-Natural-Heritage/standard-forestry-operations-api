64a9de80bfba06714833dbe0ab8e721aca210c8c
John Moore <john.moore@snh.gov.uk>
2021-07-26 15:17:02 +0100

    Add new revoke model and config
    Issue: Scottish-Natural-Heritage/Licensing#720

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,22 @@ import Sequelize from 'sequelize';
 import Application from './application.js';
 import Sett from './sett.js';
 import ApplyOther from './apply-other.js';
+import Revocation from './revocation.js';
 
 const sequelize = new Sequelize(databaseConfig.database);
 
 const database = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
   Application: Application(sequelize),
   Sett: Sett(sequelize),
-  ApplyOther: ApplyOther(sequelize)
+  ApplyOther: ApplyOther(sequelize),
+  Revocation: Revocation(sequelize)
 };
 
 database.Application.hasMany(database.Sett);
+database.Application.hasOne(database.Revocation);
 database.Sett.belongsTo(database.Application);
+database.Revocation.belongsTo(database.Application);
 
 export {database as default};
