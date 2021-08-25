e8bb6e1c4d0860881963594a761381a480262d79
murrayholmes96 <murray.holmes@nature.scot>
2021-08-25 13:39:12 +0100

    Add a clean patch input function
    Issue: Scottish-Natural-Heritage/Licensing#760

--- src/controllers/v2/application.js
+++ src/controllers/v2/application.js
@@ -1,6 +1,7 @@
 import Sequelize from 'sequelize';
 // eslint-disable-next-line unicorn/import-index, import/no-useless-path-segments
 import database from '../../models/index.js';
+import utils from 'naturescot-utils';
 
 const {Application, Returns, Sett, Revocation} = database;
 
@@ -8,62 +9,62 @@ const {Application, Returns, Sett, Revocation} = database;
  * Clean an incoming PATCH request body to make it more compatible with the
  * database and its validation rules.
  *
- * @param {any} body the incoming request's body
- * @returns {any} cleanedBody a json object that's just got our cleaned up fields on it
+ * @param {any} body The incoming request's body.
+ * @returns {any} CleanedBody a json object that's just got our cleaned up fields on it.
  */
- const cleanPatchInput = (body) => {
+const cleanPatchInput = (body) => {
   const cleanedBody = {};
 
-   // Check for the existence of each field and if found clean it if required and add to the cleanedBody object.
-   if (body.fullName) {
-     cleanedBody.fullName = body.fullName.trim();
-   }
+  // Check for the existence of each field and if found clean it if required and add to the cleanedBody object.
+  if (body.fullName) {
+    cleanedBody.fullName = body.fullName.trim();
+  }
 
-   if (body.companyOrganisation) {
-     cleanedBody.companyOrganisation = body.companyOrganisation.trim();
-   }
+  if (body.companyOrganisation) {
+    cleanedBody.companyOrganisation = body.companyOrganisation.trim();
+  }
 
-   if (body.emailAddress) {
-     cleanedBody.emailAddress = utils.recipients.validateAndFormatEmailAddress(body.emailAddress);
-   }
+  if (body.emailAddress) {
+    cleanedBody.emailAddress = utils.recipients.validateAndFormatEmailAddress(body.emailAddress);
+  }
 
-   if (body.addressLine1) {
-     cleanedBody.addressLine1 = body.addressLine1.trim();
-   }
+  if (body.addressLine1) {
+    cleanedBody.addressLine1 = body.addressLine1.trim();
+  }
 
-   if (body.addressLine2) {
-     cleanedBody.addressLine2 = body.addressLine2.trim();
-   }
+  if (body.addressLine2) {
+    cleanedBody.addressLine2 = body.addressLine2.trim();
+  }
 
-   if (body.addressTown) {
-     cleanedBody.addressTown = body.addressTown.trim();
-   }
+  if (body.addressTown) {
+    cleanedBody.addressTown = body.addressTown.trim();
+  }
 
-   if (body.addressCounty) {
-     cleanedBody.addressCounty = body.addressCounty.trim();
-   }
+  if (body.addressCounty) {
+    cleanedBody.addressCounty = body.addressCounty.trim();
+  }
 
-   if (body.addressPostcode) {
-     cleanedBody.addressPostcode = utils.postalAddress.formatPostcodeForPrinting(body.addressPostcode);
-     if (!utils.postalAddress.isaRealUkPostcode(cleanedBody.addressPostcode)) {
-       throw new Error('Invalid postcode.');
-     }
-   }
+  if (body.addressPostcode) {
+    cleanedBody.addressPostcode = utils.postalAddress.formatPostcodeForPrinting(body.addressPostcode);
+    if (!utils.postalAddress.isaRealUkPostcode(cleanedBody.addressPostcode)) {
+      throw new Error('Invalid postcode.');
+    }
+  }
 
-   if (body.phoneNumber) {
-     cleanedBody.phoneNumber = body.phoneNumber.trim();
-   }
+  if (body.phoneNumber) {
+    cleanedBody.phoneNumber = body.phoneNumber.trim();
+  }
 
-   if (body.convictions) {
-     cleanedBody.convictions = body.convictions;
-   }
+  if (body.convictions) {
+    cleanedBody.convictions = body.convictions;
+  }
 
-   if (body.createdByLicensingOfficer) {
-     cleanedBody.createdByLicensingOfficer = body.createdByLicensingOfficer();
-   }
+  if (body.createdByLicensingOfficer) {
+    cleanedBody.createdByLicensingOfficer = body.createdByLicensingOfficer();
+  }
 
-   return cleanedBody;
- };
+  return cleanedBody;
+};
 
 /**
  * An object to perform 'persistence' operations on our application objects.
@@ -106,6 +107,27 @@ const ApplicationController = {
     } catch {
       return false;
     }
+  },
+  /**
+   * Update a application in the database with partial JSON model.
+   *
+   * @param {number} id An existing application's ID.
+   * @param {any} application A JSON version of the model containing only the fields to be updated.
+   * @returns {boolean} True if the record is updated, otherwise false.
+   */
+  update: async (id, application) => {
+    // Save the new values to the database.
+    const result = await Application.update(application, {where: {id}});
+
+    // Check to make sure the saving process went OK.
+    const success = result.length > 0 && result[0] === 1;
+    if (success) {
+      // Return JSON with the updated fields on successful update.
+      return application;
+    }
+
+    // If something went wrong, return undefined to signify this.
+    return undefined;
   }
 };
 
