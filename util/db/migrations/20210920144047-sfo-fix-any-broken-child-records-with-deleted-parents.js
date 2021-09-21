c4689decf244eed491d4dc773c01774554c238e5
John Moore <john.moore@snh.gov.uk>
2021-09-21 10:07:40 +0100

    Add migration to fix broken child records
    Issue: Scottish-Natural-Heritage/Licensing#776

---
+++ util/db/migrations/20210920144047-sfo-fix-any-broken-child-records-with-deleted-parents.js
@@ -0,0 +1,51 @@
+'use strict';
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE sfo."Setts" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ApplicationId" IN (SELECT id FROM sfo."Applications" WHERE "deletedAt" IS NOT NULL)";`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE sfo."Returns" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ApplicationId" IN (SELECT id FROM sfo."Applications" WHERE "deletedAt" IS NOT NULL)";`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+    },
+
+    down: async (queryInterface, Sequelize) => {
+      // Do something here
+    }
+  };
+} else {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Setts" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ApplicationId" IN (SELECT id FROM "Applications" WHERE "deletedAt" IS NOT NULL)";`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Returns" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ApplicationId" IN (SELECT id FROM "Applications" WHERE "deletedAt" IS NOT NULL)";`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+    },
+
+    down: async (queryInterface, Sequelize) => {
+      // Do something here
+    }
+  };
+}
