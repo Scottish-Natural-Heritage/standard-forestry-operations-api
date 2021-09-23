d993d64752cc473d158dea04f0ca27bd4271ff53
John Moore <john.moore@snh.gov.uk>
2021-09-23 10:59:13 +0100

    Add delete endpoins to v2router
    Issue: Scottish-Natural-Heritage/Licensing#890
    Issue: Scottish-Natural-Heritage/Licensing#891

--- src/v2-router.js
+++ src/v2-router.js
@@ -243,6 +243,7 @@ v2router.post('/applications/:id/returns', async (request, response) => {
 });
 
 // Allow an API consumer to save a application against an allocated but un-assigned application number.
+// No PUT endpoint is required as we now use a PATCH method of updating details.
 v2router.put('/applications/:id', async (request, response) => {
   return response.status(501).send({message: 'Not implemented.'});
 });
@@ -310,7 +311,9 @@ v2router.patch('/applications/:id', async (request, response) => {
   }
 });
 
-// Allow an API consumer to delete a application.
+/**
+ * DELETEs a single application and all child records.
+ */
 v2router.delete('/applications/:id', async (request, response) => {
   try {
     // Try to parse the incoming ID to make sure it's really a number.
@@ -321,14 +324,16 @@ v2router.delete('/applications/:id', async (request, response) => {
 
     // Clean up the user's input before we store it in the database.
     const cleanObject = cleanRevokeInput(existingId, request.body);
+    // Attempt to delete the application and all child records.
     // eslint-disable-next-line unicorn/prevent-abbreviations
     const deleteApplication = await Application.delete(existingId, cleanObject);
 
+    // If we were unable to delete the application we need to return a 500 with a suitable error message.
     if (deleteApplication === false) {
       return response.status(500).send({message: `Could not delete Application ${existingId}.`});
     }
 
-    // If they are, send back true.
+    // If we were able to delete the application we need to return a 200.
     return response.status(200).send();
   } catch (error) {
     // If anything goes wrong (such as a validation error), tell the client.
@@ -338,13 +343,41 @@ v2router.delete('/applications/:id', async (request, response) => {
 
 // Save an incoming email address so we can email them later once the apply on
 // behalf of others service is up and running.
+// this may need done in the future but currently we cant apply on behalf of.
 v2router.post('/apply-other', async (request, response) => {
   return response.status(501).send({message: 'Not implemented.'});
 });
 
-// Allow an API consumer to delete a sett against a application.
+/**
+ * DELETEs a single Sett.
+ */
 v2router.delete('/applications/:id/setts/:settId', async (request, response) => {
-  return response.status(501).send({message: 'Not implemented.'});
+  try {
+    // Try to parse the incoming application ID to make sure it's really a number.
+    const existingId = Number(request.params.id);
+    if (Number.isNaN(existingId)) {
+      return response.status(404).send({message: `Application ${request.params.id} not valid.`});
+    }
+
+    // Try to parse the incoming Sett ID to make sure it's really a number.
+    const existingSettId = Number(request.params.settId);
+    if (Number.isNaN(existingSettId)) {
+      return response.status(404).send({message: `Sett ${request.params.settId} not valid.`});
+    }
+
+    // Attempt to delete the sett.
+    const deleteSett = await Sett.delete(existingSettId);
+    // If we were unable to delete the sett we need to return a 500 with a suitable error message.
+    if (deleteSett === false) {
+      return response.status(500).send({message: `Could not delete Sett ${existingSettId}.`});
+    }
+
+    // If we were able to delete the sett we need to return a 200.
+    return response.status(200).send();
+  } catch (error) {
+    // If anything goes wrong (such as a validation error), tell the client.
+    return response.status(500).send({error});
+  }
 });
 
 /**
