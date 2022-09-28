b1a1d0bd95ec35365374d34bcebbde15bd85e9ec
Paul Cox <paul.cox@nature.scot>
2022-09-28 08:24:06 +0100

    Rename old returns table and create new returns table migration scripts. Issue Licensing 1470

--- util/db/migrations/20220927072808-sfo-rename-old-returns-table.js
+++ util/db/migrations/20220927072808-sfo-rename-old-returns-table.js
@@ -4,22 +4,22 @@ const databaseConfig = require('../../../src/config/database.js');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn(
+    await queryInterface.renameTable(
       {
         schema: databaseConfig.database.schema,
-        tableName: 'Applications'
+        tableName: 'Returns'
       },
-      'createdByLicensingOfficer',
-      Sequelize.STRING
+      'ReturnsOld'
     );
   },
-  async down(queryInterface) {
-    await queryInterface.removeColumn(
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.renameTable(
       {
         schema: databaseConfig.database.schema,
-        tableName: 'Applications'
+        tableName: 'ReturnsOld'
       },
-      'createdByLicensingOfficer'
+      'Returns'
     );
   }
 };
