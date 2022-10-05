eb2e43d1ad12918b155c80e05ad5a8c644a6844c
Paul Cox <paul.cox@nature.scot>
2022-10-05 11:35:38 +0100

    Comment the code. Issue Licensing 1414

--- src/controllers/v2/returns.js
+++ src/controllers/v2/returns.js
@@ -10,6 +10,14 @@ const createDisplayDate = (date) => {
   return date.toLocaleDateString('en-GB', {year: 'numeric', month: 'numeric', day: 'numeric'});
 };
 
+/**
+ * Creates a single string of details of the uploaded sett photographs for display
+ * in the Notify email.
+ *
+ * @param {any} uploadDetails The filenames of the uploaded files.
+ * @param {string[]} settNames The names of the setts associated with the photographs.
+ * @returns {string} The string to use in the Notify email.
+ */
 const createDisplayablePhotoDetails = (uploadDetails, settNames) => {
   const photoDetails = [];
 
@@ -24,6 +32,17 @@ const createDisplayablePhotoDetails = (uploadDetails, settNames) => {
   return photoDetails.join('\n');
 };
 
+/**
+ * Sends the details of the return to the Notify API, asking it to send off an email.
+ * The email sent is the one used if the licence holder used the licence.
+ *
+ * @param {string} notifyApiKey
+ * @param {any} application
+ * @param {any} newReturn
+ * @param {string[]} settNames
+ * @param {any} uploadDetails
+ * @param {string} emailAddress
+ */
 const sendReturnEmailUsedLicence = async (
   notifyApiKey,
   application,
@@ -43,7 +62,9 @@ const sendReturnEmailUsedLicence = async (
           startDate: createDisplayDate(new Date(newReturn.startDate)),
           endDate: createDisplayDate(new Date(newReturn.endDate)),
           compliance: newReturn.compliance ? 'Yes' : 'No',
-          moreDetails: newReturn.complianceDetails ? newReturn.complianceDetails : 'No additional compliance details provided.',
+          moreDetails: newReturn.complianceDetails
+            ? newReturn.complianceDetails
+            : 'No additional compliance details provided.',
           photosDetails: createDisplayablePhotoDetails(uploadDetails, settNames)
         },
         reference: `NS-SFO-${application.id}`,
@@ -56,6 +77,14 @@ const sendReturnEmailUsedLicence = async (
   }
 };
 
+/**
+ * Sends the details of the return to the Notify API, asking it to send off an email.
+ * The email sent is the one used if the licence holder did not use the licence.
+ *
+ * @param {string} notifyApiKey
+ * @param {any} application
+ * @param {string} emailAddress
+ */
 const sendReturnEmailNotUsedLicence = async (notifyApiKey, application, emailAddress) => {
   if (notifyApiKey) {
     try {
