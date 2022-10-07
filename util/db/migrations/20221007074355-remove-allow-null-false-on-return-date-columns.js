0e82a9d2306678e6846c7f2be85fbbb920d66336
Paul Cox <paul.cox@nature.scot>
2022-10-07 11:00:48 +0100

    Update migration to work correctly with PostgreSQL. Issue Licensing 1470

--- util/db/migrations/20221007074355-remove-allow-null-false-on-return-date-columns.js
+++ util/db/migrations/20221007074355-remove-allow-null-false-on-return-date-columns.js
@@ -1,22 +1,26 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.changeColumn('Returns', 'startDate', {
+      type: Sequelize.DATE,
       allowNull: true
     });
 
     await queryInterface.changeColumn('Returns', 'endDate', {
+      type: Sequelize.DATE,
       allowNull: true
     });
   },
 
-  async down(queryInterface) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.changeColumn('Returns', 'endDate', {
+      type: Sequelize.DATE,
       allowNull: false
     });
 
     await queryInterface.changeColumn('Returns', 'startDate', {
+      type: Sequelize.DATE,
       allowNull: false
     });
   }
