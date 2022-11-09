e2cdbc0730c8cc17c99cb6160abb0c14e3ac6a4d
Paul Cox <paul.cox@nature.scot>
2022-11-09 13:07:59 +0000

    Add Revocation model to findAll function. Issue Licensing 1377

--- src/controllers/v1/application.js
+++ src/controllers/v1/application.js
@@ -120,7 +120,7 @@ const ApplicationController = {
    * @returns  {Sequelize.Model} All existing applications.
    */
   async findAll() {
-    return Application.findAll({include: Sett});
+    return Application.findAll({paranoid: false, include: [{model: Sett}, {model: Revocation, paranoid: false}]});
   },
 
   /**
