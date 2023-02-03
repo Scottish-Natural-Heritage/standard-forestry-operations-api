4b5fa3df04cf277c2ade1873a8405f48b3c6a088
Paul Cox <paul.cox@nature.scot>
2023-02-03 13:58:38 +0000

    Add axios dependency and first scheduled task, reminder on expired licence with no returns. Issues 1489, 1490

--- src/scheduled-tasks.js
+++ src/scheduled-tasks.js
@@ -1,14 +1,29 @@
 // Use node-cron for scheduled tasks.
 import cron from 'node-cron';
 
+// Use to make HTTP calls.
+import axios from 'axios';
+
 /**
  * Start up node-cron.
  */
 const initScheduledJobs = () => {
-  const scheduledJobFunction = cron.schedule('5 * * * * *', () => {
+  const scheduledJobFunction = cron.schedule('0 6 * * *', () => {
     console.log("Triggering cron job(s).");
 
+    // Get the date.
+    const currentDate = new Date();
+
     // Tasks here.
+
+    // Check for expired licences with not returns submitted, on the 1st of January and 1st of February.
+    if (currentDate.getDate() === 1 && (currentDate.getMonth() === 0 || currentDate.getMonth === 1)) {
+      try {
+        await axios.post(`http://localhost:3017${config.pathPrefix}/expired-no-return-reminder`);
+      } catch (error) {
+        console.error(JsonUtils.unErrorJson(error));
+      }
+    }
 
     console.log('Ending cron job(s).');
   });
